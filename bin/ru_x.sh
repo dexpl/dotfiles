16d24bee71e47810f9c68b7cdb41ff927cfbb61a
Vadim Raskhozhev <iamdexpl@gmail.com>
2019-11-11 15:47:18 +0300

    ru_x.sh: first clear any options, then set

--- bin/ru_x.sh
+++ bin/ru_x.sh
@@ -4,7 +4,7 @@
 #setxkbmap -layout us
 #setxkbmap -layout us,ru -option -option grp:alts_toggle,grp:shifts_toggle,grp:shift_caps_toggle,grp_led:scroll,terminate:ctrl_alt_bksp,compose:rwin -symbols "+kpdl(dotoss)+kpdl(dotoss):2" &
 # Еще более гребаный костыль, я хз насколько он рабочий. Как минимум, он вроде бы работает в ком. строке (за искл. alts_toggle, к-рые, впрочем, не работают уже хз сколько вообще никак)
-#setxkbmap -option
+setxkbmap -option
 setxkbmap -layout us,ru
 setxkbmap -option grp_led:scroll,terminate:ctrl_alt_bksp,compose:rwin
 # For notebook keyboards lacking rwin
