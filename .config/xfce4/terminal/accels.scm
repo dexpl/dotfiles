5c7fea6102b53f1ebcebcb24a08ec0e4b7b76e7e
Vadim Raskhozhev <iamdexpl@gmail.com>
2021-03-17 16:57:02 +0300

    .config/xfce4/terminal/accels.scm: added last-active-tab hotkey

--- .config/xfce4/terminal/accels.scm
+++ .config/xfce4/terminal/accels.scm
@@ -30,7 +30,7 @@
 ; (gtk_accel_path "<Actions>/terminal-window/file-menu" "")
 ; (gtk_accel_path "<Actions>/terminal-window/fullscreen" "F11")
 ; (gtk_accel_path "<Actions>/terminal-window/help-menu" "")
-; (gtk_accel_path "<Actions>/terminal-window/last-active-tab" "")
+(gtk_accel_path "<Actions>/terminal-window/last-active-tab" "<Alt>Down")
 ; (gtk_accel_path "<Actions>/terminal-window/paste" "<Primary><Shift>v")
 ; (gtk_accel_path "<Actions>/terminal-window/paste-selection" "")
 ; (gtk_accel_path "<Actions>/terminal-window/preferences" "")
